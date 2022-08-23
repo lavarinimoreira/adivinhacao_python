20dab29aa2e33e39337370d082ac1df8b3b86d41
Gabriel Lavarini <lavarinimoreira@gmail.com>
2022-08-22 21:41:47 -0300

    Adding a while loop and "acertou" and "enforcou" variables

--- forca.py
+++ forca.py
@@ -15,6 +15,14 @@ def jogar():
     print("#  Bem vindo ao jogo da Forca!  #")
     print("+-------------------------------+\n")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou  = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
     print("Fim de jogo.")
 
 if(__name__ == "__main__"):
