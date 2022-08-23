f6d7ff2ffe5915493ad0cef97c62d40bc663abb6
Gabriel Lavarini <lavarinimoreira@gmail.com>
2022-08-23 13:43:05 -0300

    Adding a for loop to search the charactere(s)

--- forca.py
+++ forca.py
@@ -21,6 +21,16 @@ def jogar():
     acertou  = False
 
     while(not enforcou and not acertou):
+
+        chute = input("Forneça uma letra: ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra '{}' na posição {}".format(letra.upper(), index))
+            index = index + 1
+
         print("jogando...")
 
     print("Fim de jogo.")
